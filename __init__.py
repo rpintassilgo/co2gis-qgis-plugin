99bd40f38823ef3510c714f61b66e51e5ed861a5
Rodrigo Pintassilgo <rodrigo.pintassilgo@glartek.com>
2024-11-24 23:50:46 +0000

    feat: main init file

--- __init__.py
+++ __init__.py
@@ -6,9 +6,29 @@
  Calculate least cost co2 pipeline
  ***************************************************************************/
 """
+from PyQt5.QtWidgets import QAction
+from .src.dialog import Dialog
 
-from .least_cost_pipeline import LeastCostPipelinePlugin
+class LeastCostPipelinePlugin:
+    def __init__(self, iface):
+        self.iface = iface
+        self.dialog = None
+        self.action = None
 
+    def initGui(self):
+        self.action = QAction("Least Cost Pipeline", self.iface.mainWindow())
+        self.action.triggered.connect(self.show_dialog)
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu("&Least Cost Pipeline", self.action)
+
+    def unload(self):
+        self.iface.removePluginMenu("&Least Cost Pipeline", self.action)
+        self.iface.removeToolBarIcon(self.action)
+
+    def show_dialog(self):
+        if not self.dialog:
+            self.dialog = Dialog()
+        self.dialog.show()
+        
 def classFactory(iface):
-    """Load LeastCostPipelinePlugin class."""
     return LeastCostPipelinePlugin(iface)
